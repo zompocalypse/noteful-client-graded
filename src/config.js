27a7eec6d4afda2e02231d1a2b38477720b19046
Jason Stankevich <jason.stankevich@gmail.com>
2020-09-29 17:15:11 -0400

    deploying again

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://stormy-lake-84367.herokuapp.com',
+  API_ENDPOINT: 'https://stormy-lake-84367.herokuapp.com/api',
   API_KEY: process.env.REACT_APP_API_KEY
 }
